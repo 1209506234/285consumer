4a703c9d23ec47a1410c9510d971ff211b886661
1209506234 <1209506234@qq.com>
2020-03-02 09:54:57 +0800

    消费者项目上传github

---
+++ src/main/java/com/zb/consumer/ConsumerApplication.java
@@ -0,0 +1,28 @@
+package com.zb.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+//@EnableHystrix
+public class ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+    @Bean//把返回的对象加入到spring容器
+    //@LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();//访问远端接口的
+    }
+}
